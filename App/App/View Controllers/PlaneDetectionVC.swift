b204ff9097d6e109f0fa917c31d5b15b46668325
Gareth Fuller <gareth.fuller@clark.de>
2017-12-13 21:51:44 +0100

    More on plane detection

---
+++ App/App/View Controllers/PlaneDetectionVC.swift
@@ -0,0 +1,64 @@
+//
+//  PlaneDetection.swift
+//  App
+//
+//  Created by Gareth on 13.12.17.
+//  Copyright © 2017 Gareth. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class PlaneDetectionVC: UIViewController, ARSCNViewDelegate {
+    
+    var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // dynamically inject scene view into here
+        sceneView = ARSCNView(frame: self.view.frame)
+        
+        // show debug options also!
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        
+        // add to the main view
+        self.view.addSubview(sceneView)
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // This means we get notified when we get planes detected!
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+}
